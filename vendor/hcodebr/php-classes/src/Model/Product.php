465643402223f2602605303736da64b295bc6632
Leandro Nonato <lenonato@hotmail.com>
2018-01-18 17:37:51 -0200

    erro

--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -20,27 +20,24 @@ class Product extends Model {
 	public function save()
 	{
 
-		$sql = new Sql();
-	
-		$results = $sql->select("CALL sp_products_save(:idproduct, :desproduct, :vlprice, :vlwidth, :vlheight, :vllenght, :vlweight, :desurl)", array(
-			":idproduct"=>$this->getidproduct(),
-			":desproduct"=>$this->getdesproduct(),
-			":vlprice"=>$this->getvlprice(),
-			":vlwidth"=>$this->getvlwidth(),
-			":vlheight"=>$this->getvlheight(),
-			":vllenght"=>$this->getvllenght(),
-			":vlweight"=>$this->getvlweight(),
-			":desurl"=>$this->getdesurl()
+    $sql = new Sql();
 
-		));
+    $results = $sql->select("CALL sp_products_save(:idproduct, :desproduct, :vlprice, :vlwidth, :vlheight, :vllength, :vlweight, :desurl)", array(
+        ":idproduct"=>$this->getidproduct(),
+        ":desproduct"=>$this->getdesproduct(),
+        ":vlprice"=>$this->getvlprice(),
+        ":vlwidth"=>$this->getvlwidth(),
+        ":vlheight"=>$this->getvlheight(),
+        ":vllength"=>$this->getvllength(),
+        ":vlweight"=>$this->getvlweight(),
+        ":desurl"=>$this->getdesurl()
+    ));
 
-		$this->setData($results[0]);
-
-		Category::updateFile();
+    	$this->setData($results[0]);
 
 	}
 
-	public function get($idcategory)
+	public function get($idproduct)
 	{
 
 	$sql = new Sql();
